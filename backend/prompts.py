6083db0d174a61185ba3252b5e7f9c0d509b638f
Torsten Werner <torsten.werner@westernacher.com>
2025-12-29 14:10:16 +0100

    improve prompt

--- backend/prompts.py
+++ backend/prompts.py
@@ -5,6 +5,7 @@ Include the pronunciation of the word.
 If this word has multiple meanings, explain all of them.
 Provide a list of synonyms and related words as well.
 Show common phrases and examples using this word.
+Skip any introduction text in your answer.
 The word to explain is: {term}
 """
 
